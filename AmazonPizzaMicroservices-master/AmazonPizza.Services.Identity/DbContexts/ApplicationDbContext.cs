481ef093c70ababcc94b31561ca82c0ba524b6e4
fhs2020 <flavioh007@gmail.com>
2021-11-28 10:49:12 -0300

    adding Identity resources

---
+++ AmazonPizzaMicroservices-master/AmazonPizza.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AmazonPizza.Services.Identity.Models;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmazonPizza.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+
+
+}
