cc83bf43dedf3b92bcc3e4b1fff55818fa5bc268
fhs2020 <flavioh007@gmail.com>
2022-04-08 22:50:24 -0300

    adding ViewModel

--- AmazonPizza.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AmazonPizza.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
