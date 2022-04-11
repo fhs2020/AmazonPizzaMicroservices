8d059bdab7ec824fbd10fa1162db8a29cd88ca29
fhs2020 <flavioh007@gmail.com>
2022-04-11 17:11:07 -0300

    adding Advertisement Configuration View Model

---
+++ AmazonasPizza-master/StarterProject.WebSite/Models/ProductAdvertiseConfigurationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StarterProject.WebSite.Models
+{
+    public class ProductAdvertiseConfigurationViewModel
+    {
+        public string FirstCarrouselVariableOne { get; set; }
+        public string FirstCarrouselVariableTwo { get; set; }
+        public string FirstCarrouselVariableThree { get; set; }
+    }
+}
